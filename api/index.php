dbcc1d79d6d36a1f6587dda0cb065a7ed9f8b0f4
LorentzaZweena <ariva02zweena@gmail.com>
2026-07-09 14:38:50 +0700

    j

--- api/index.php
+++ api/index.php
@@ -1,3 +1,4 @@
 <?php
 
+// Langsung bypass eksekusi ke public index bawaan Laravel
 require __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -1,20 +1,30 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
+    config(['app.configuration_cached' => false]);
+    config(['app.routes_cached' => false]);
+    config(['app.services_cached' => false]);
+    config(['app.events_cached' => false]);
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+} else {
+    require __DIR__.'/vendor/autoload.php';
+}
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+if (file_exists(__DIR__.'/../bootstrap/app.php')) {
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+} else {
+    $app = require_once __DIR__.'/bootstrap/app.php';
+}
 
-$app->handleRequest(Request::capture());
+$response = $app->handle(Request::capture());
+
+$response->send();
+
+$app->terminate(Request::capture(), $response);
