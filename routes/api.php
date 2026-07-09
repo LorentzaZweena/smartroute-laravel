02025d31a9a79838899aada10e1286eb96f13a9d
LorentzaZweena <ariva02zweena@gmail.com>
2026-07-09 16:47:14 +0700

    y

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\RouteAiController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RouteAiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/api/calculate-route', [RouteAiController::class, 'calculateRoute']);
+Route::post('/calculate-route', [RouteAiController::class, 'calculateRoute']);
