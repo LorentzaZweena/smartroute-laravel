7fb8d89aec6613a7add644f6ecef43c46ebd64bd
LorentzaZweena <ariva02zweena@gmail.com>
2026-07-09 18:32:30 +0700

    tes mapid #4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\RouteAiController;
 
 Route::get('/', function () {
@@ -8,3 +9,15 @@ Route::get('/', function () {
 });
 
 Route::post('/calculate-route', [RouteAiController::class, 'calculateRoute']);
+Route::get('/mapid-proxy/style.json', function () {
+    $apiKey = '0cd87839439d453e83fc7da1547fafdb';
+    $response = Http::get("https://geo.mapid.io/tiles/v1/styles/default/style.json?key={$apiKey}");
+    
+    if ($response->successful()) {
+        return response($response->body())
+            ->header('Content-Type', 'application/json')
+            ->header('Access-Control-Allow-Origin', '*');
+    }
+    
+    return response()->json(['error' => 'Gagal mengambil basemap'], 500);
+});
