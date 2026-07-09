92736b278ecbf424e06982395fe71b05ee0683bb
LorentzaZweena <ariva02zweena@gmail.com>
2026-07-09 14:16:24 +0700

    f

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -20,3 +20,16 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*'),
         );
     })->create();
+
+    if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
+        $storagePath = '/tmp/storage';
+        if (!is_dir($storagePath)) {
+            mkdir($storagePath . '/framework/views', 0755, true);
+            mkdir($storagePath . '/framework/cache', 0755, true);
+            mkdir($storagePath . '/framework/sessions', 0755, true);
+            mkdir($storagePath . '/logs', 0755, true);
+        }
+        $app->useStoragePath($storagePath);
+    }
+
+return $app;
