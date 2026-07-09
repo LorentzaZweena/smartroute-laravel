bd41209a7296abd6ccdca51063f876929c317b2d
LorentzaZweena <ariva02zweena@gmail.com>
2026-07-09 16:22:54 +0700

    u

--- public/index.php
+++ public/index.php
@@ -3,24 +3,31 @@
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-if (isset($_SERVER['VERCEL_URL']) || isset($_ENV['VERCEL_URL'])) {
-    $tmpCacheDir = '/tmp/storage/bootstrap/cache';
-    if (!file_exists($tmpCacheDir)) {
-        mkdir($tmpCacheDir, 0755, true);
-    }
-    
-    putenv("APP_PACKAGES_CACHE={$tmpCacheDir}/packages.php");
-    $_ENV['APP_PACKAGES_CACHE'] = "{$tmpCacheDir}/packages.php";
-    $_SERVER['APP_PACKAGES_CACHE'] = "{$tmpCacheDir}/packages.php";
-}
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
+// Jalankan Autoloader
 require __DIR__.'/../vendor/autoload.php';
+
+// Ambil instansiasi aplikasi asli
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// --- TRICK PAMUNGKAS VERCEL SERVERLESS ---
+// Paksa semua file manifest (packages, services, config) ditulis ke /tmp
+if (isset($_SERVER['VERCEL_URL']) || isset($_ENV['VERCEL_URL'])) {
+    $targetCacheDir = '/tmp/storage/bootstrap/cache';
+    if (!file_exists($targetCacheDir)) {
+        mkdir($targetCacheDir, 0755, true);
+    }
+
+    // Paksa Laravel mengubah lokasi penyimpanan bootstrap path secara runtime
+    $app->useBootstrapPath($targetCacheDir);
+    
+    // Backup env cadangan demi keamanan internal framework
+    putenv("APP_packages_CACHE={$targetCacheDir}/packages.php");
+    putenv("APP_services_CACHE={$targetCacheDir}/services.php");
+}
+// -----------------------------------------
+
+// Proses Request seperti biasa
 $response = $app->handle(Request::capture());
 
 $response->send();
