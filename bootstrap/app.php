8c2a212b7ac39d373bae58d8bc4a29b118fb2fd5
LorentzaZweena <ariva02zweena@gmail.com>
2026-07-09 16:19:03 +0700

    t

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,26 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// 1. BAJAK PATH CACHE UNTUK VERCEL (Tambahkan blok ini di paling atas)
-if (isset($_SERVER['VERCEL_URL']) || env('LARAVEL_STORAGE_PATH')) {
-    $app = new Application(
-        $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-    );
-
-    // Paksa Laravel menulis cache bootstrap dan storage ke folder /tmp yang writable
-    $app->useStoragePath('/tmp/storage');
-    $app->bootstrapWith([
-        \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
-    ]);
-    
-    // Set environment variable secara runtime agar config cache aman
-    $_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/framework/config.php';
-    $_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/framework/routes.php';
-    $_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/framework/services.php';
-    $_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/framework/packages.php';
-}
-
-// 2. CONFIG STRUKTUR BAWAAN LARAVEL (Biarkan bagian bawah ini tetap seperti aslinya)
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
--- public/index.php
+++ public/index.php
@@ -3,25 +3,23 @@
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
-if (isset($_SERVER['LAMBDA_TASK_ROOT'])) {
-    config(['app.configuration_cached' => false]);
-    config(['app.routes_cached' => false]);
-    config(['app.services_cached' => false]);
-    config(['app.events_cached' => false]);
+if (isset($_SERVER['VERCEL_URL']) || isset($_ENV['VERCEL_URL'])) {
+    $tmpCacheDir = '/tmp/storage/bootstrap/cache';
+    if (!file_exists($tmpCacheDir)) {
+        mkdir($tmpCacheDir, 0755, true);
+    }
+    
+    putenv("APP_PACKAGES_CACHE={$tmpCacheDir}/packages.php");
+    $_ENV['APP_PACKAGES_CACHE'] = "{$tmpCacheDir}/packages.php";
+    $_SERVER['APP_PACKAGES_CACHE'] = "{$tmpCacheDir}/packages.php";
 }
 
-if (file_exists(__DIR__.'/../vendor/autoload.php')) {
-    require __DIR__.'/../vendor/autoload.php';
-} else {
-    require __DIR__.'/vendor/autoload.php';
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
-if (file_exists(__DIR__.'/../bootstrap/app.php')) {
-    $app = require_once __DIR__.'/../bootstrap/app.php';
-} else {
-    $app = require_once __DIR__.'/bootstrap/app.php';
-}
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $response = $app->handle(Request::capture());
 
