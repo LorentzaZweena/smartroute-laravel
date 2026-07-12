40cae6271ee5b895cb997a45043ec3e07ee2253c
LorentzaZweena <ariva02zweena@gmail.com>
2026-07-12 14:26:40 +0700

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,21 +20,22 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
-    ->tap(function ($app) use ($storagePath) {
-        if (isset($storagePath) && is_dir($storagePath)) {
-            $app->useStoragePath($storagePath);
-        }
-    })
     ->create();
+
+if (isset($storagePath) && is_dir($storagePath)) {
+    $app->useStoragePath($storagePath);
+}
+
+return $app;
